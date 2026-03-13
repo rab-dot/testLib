edecd7966620795d623b771d74736e00ec12d226
rab-dot <rabdot3@gmail.com>
2026-03-13 15:00:29 +0300

    docs: script config

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -65,9 +65,10 @@ function createGitTag(version) {
 
 function generateChangelog() {
   try {
-    execSync('yarn conventional-changelog -i CHANGELOG.md -s', {
-      stdio: 'inherit',
-    })
+    execSync(
+      'yarn conventional-changelog -p conventionalcommits -i CHANGELOG.md -s --config .versionrc.json',
+      { stdio: 'inherit' }
+    )
   } catch (error) {
     console.warn('⚠️  Changelog generation had issues, but continuing...')
   }
